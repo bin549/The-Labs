f1c94f90d500d29ca5dbc4d7ac28773ce8f8b88c
bin549 <549288160@qq.com>
2025-12-25 23:31:50 +0800

    chore: 欧姆定律流程

--- scenes/Experiments/OhmsLawExperiment/OhmsLawExperiment.cs
+++ scenes/Experiments/OhmsLawExperiment/OhmsLawExperiment.cs
@@ -23,8 +23,13 @@ public partial class OhmsLawExperiment : StepExperimentLabItem<OhmsLawExperiment
     [Export] public NodePath ConnectionManagerPath { get; set; } = new NodePath("/root/World/ConnectionManager");
     [Export] private Area3D switchArea;
     [Export] private AnimationPlayer switchAnimationPlayer;
+    [Export] private ConnectableNode potentiometer;
     private ConnectionManager connectionManager;
     private bool isSwitchClosed = false;
+    private bool isDraggingPotentiometer = false;
+    private bool isPotentiometerHovered = false;
+    private Vector3 potentiometerInitialPosition;
+    private Vector2 dragStartMousePosition;
 
     public override void _Ready() {
         base._Ready();
@@ -32,6 +37,13 @@ public partial class OhmsLawExperiment : StepExperimentLabItem<OhmsLawExperiment
         base.InitializeStepExperiment();
         this.ResolveConnectionManager();
         this.SetupSwitchArea();
+        this.InitializePotentiometer();
+    }
+
+    private void InitializePotentiometer() {
+        if (this.potentiometer != null) {
+            this.potentiometerInitialPosition = this.potentiometer.GlobalPosition;
+        }
     }
 
     private void SetupSwitchArea() {
@@ -80,7 +92,7 @@ public partial class OhmsLawExperiment : StepExperimentLabItem<OhmsLawExperiment
     private void SetConnectionsColorToGreen() {
         this.ResolveConnectionManager();
         if (this.connectionManager != null) {
-            this.connectionManager.SetConnectionsColorInExperiment(this, Colors.Green);
+            this.connectionManager.SetConnectionsColorInExperiment(this, Colors.DarkGreen);
         }
     }
 
@@ -91,11 +103,13 @@ public partial class OhmsLawExperiment : StepExperimentLabItem<OhmsLawExperiment
             Input.MouseMode = Input.MouseModeEnum.Visible;
         }
         this.UpdateConnectionManagerState();
+        this.UpdatePotentiometerState();
     }
 
     protected override void OnStepChanged(OhmsLawExperimentStep previousStep, OhmsLawExperimentStep newStep) {
         base.OnStepChanged(previousStep, newStep);
         this.UpdateConnectionManagerState();
+        this.UpdatePotentiometerState();
         if (newStep == OhmsLawExperimentStep.Step02) {
             this.isSwitchClosed = false;
         }
@@ -106,9 +120,17 @@ public partial class OhmsLawExperiment : StepExperimentLabItem<OhmsLawExperiment
         this.EnableConnectionManager(shouldEnable);
     }
 
+    private void UpdatePotentiometerState() {
+    }
+
     public override void ExitInteraction() {
         base.isInteracting = false;
         this.EnableConnectionManager(false);
+        if (this.isPotentiometerHovered && this.potentiometer != null) {
+            this.potentiometer.OnHoverExit();
+            this.isPotentiometerHovered = false;
+        }
+        this.isDraggingPotentiometer = false;
         base.ExitInteraction();
     }
 
@@ -120,6 +142,108 @@ public partial class OhmsLawExperiment : StepExperimentLabItem<OhmsLawExperiment
         if (this.currentStep == OhmsLawExperimentStep.Step01) {
             this.CheckConnectionCount();
         }
+        if (this.isDraggingPotentiometer && this.currentStep == OhmsLawExperimentStep.Step03) {
+            this.UpdatePotentiometerDrag();
+        }
+    }
+
+    public override void _Input(InputEvent @event) {
+        base._Input(@event);
+        if (!base.isInteracting) {
+            return;
+        }
+        if (this.currentStep != OhmsLawExperimentStep.Step03) {
+            return;
+        }
+        if (this.potentiometer == null) {
+            return;
+        }
+        if (@event is InputEventMouseMotion motionEvent) {
+            this.HandlePotentiometerMouseMotion(motionEvent.Position);
+        }
+        if (@event is InputEventMouseButton mouseButton) {
+            if (mouseButton.ButtonIndex == MouseButton.Left) {
+                if (mouseButton.Pressed && !mouseButton.IsEcho()) {
+                    this.HandlePotentiometerMouseDown(mouseButton.Position);
+                } else if (!mouseButton.Pressed && this.isDraggingPotentiometer) {
+                    this.HandlePotentiometerMouseUp();
+                }
+            }
+        }
+    }
+
+    private void HandlePotentiometerMouseMotion(Vector2 mousePosition) {
+        bool isOver = this.IsMouseOverPotentiometer(mousePosition);
+        if (isOver != this.isPotentiometerHovered) {
+            this.isPotentiometerHovered = isOver;
+            if (this.isPotentiometerHovered) {
+                this.potentiometer.OnHoverEnter();
+            } else {
+                this.potentiometer.OnHoverExit();
+            }
+        }
+    }
+
+    private void HandlePotentiometerMouseDown(Vector2 mousePosition) {
+        if (this.IsMouseOverPotentiometer(mousePosition)) {
+            this.isDraggingPotentiometer = true;
+            this.dragStartMousePosition = mousePosition;
+            this.potentiometerInitialPosition = this.potentiometer.GlobalPosition;
+            GetViewport().SetInputAsHandled();
+        }
+    }
+
+    private void HandlePotentiometerMouseUp() {
+        this.isDraggingPotentiometer = false;
+    }
+
+    private bool IsMouseOverPotentiometer(Vector2 mousePosition) {
+        var camera = GetViewport().GetCamera3D();
+        if (camera == null) {
+            return false;
+        }
+        var from = camera.ProjectRayOrigin(mousePosition);
+        var to = from + camera.ProjectRayNormal(mousePosition) * 1000f;
+        var query = PhysicsRayQueryParameters3D.Create(from, to);
+        query.CollideWithBodies = true;
+        query.CollideWithAreas = true;
+        query.CollisionMask = 1 << 19;
+        var spaceState = GetWorld3D().DirectSpaceState;
+        var result = spaceState.IntersectRay(query);
+        
+        if (result == null || !result.ContainsKey("collider")) {
+            return false;
+        }
+        var collider = result["collider"].As<Node>();
+        if (collider == null) {
+            return false;
+        }
+        Node current = collider;
+        int depth = 0;
+        const int maxDepth = 10;
+        while (current != null && depth < maxDepth) {
+            if (current == this.potentiometer) {
+                return true;
+            }
+            current = current.GetParent();
+            depth++;
+        }
+        return false;
+    }
+
+    private void UpdatePotentiometerDrag() {
+        var currentMousePos = GetViewport().GetMousePosition();
+        var mouseDelta = currentMousePos - this.dragStartMousePosition;
+        Node3D parent = this.potentiometer.GetParent() as Node3D;
+        Vector3 localZAxis;
+        if (parent != null) {
+            localZAxis = parent.GlobalTransform.Basis.Z;
+        } else {
+            localZAxis = Vector3.Forward;
+        }
+        var movementDistance = -mouseDelta.X * 0.01f;
+        var newPosition = this.potentiometerInitialPosition + localZAxis * movementDistance;
+        this.potentiometer.GlobalPosition = newPosition;
     }
 
     private void CheckConnectionCount() {
